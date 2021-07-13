eba42ff28c1841365dd97cd62fad44050544c38a
DESKTOP-8657Q1S\정봉준 <sjwnrdjtj@naver.com>
2021-07-14 00:44:46 +0900

    유니티 교과서 8장 (Library 제외)

---
+++ 8장 레벨 디자인/AppleCatch/Assets/GameDirector.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject timerText;
+    GameObject pointText;
+    float time = 30.0f;
+    int point = 0;
+    GameObject generator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.timerText = GameObject.Find("Time");
+        this.pointText = GameObject.Find("Point");
+        this.generator = GameObject.Find("ItemGenerator");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.time -= Time.deltaTime;
+
+        if (this.time < 0)
+        {
+            this.time = 0;
+            this.generator.GetComponent<ItemGenerator>().SetParameter(10000.0f, 0, 0);
+        }
+        else if (0 <= this.time && this.time < 5)
+        {
+            this.generator.GetComponent<ItemGenerator>().SetParameter(0.7f, -0.04f, 3);
+        }
+        else if (5 <= this.time && this.time < 12)
+        {
+            this.generator.GetComponent<ItemGenerator>().SetParameter(0.8f, -0.06f, 6);
+        }
+        else if (10 <= this.time && this.time < 23)
+        {
+            this.generator.GetComponent<ItemGenerator>().SetParameter(0.8f, -0.04f, 4);
+        }
+        else if (20 <= this.time && this.time < 30)
+        {
+            this.generator.GetComponent<ItemGenerator>().SetParameter(1.0f, -0.03f, 2);
+        }
+
+        this.timerText.GetComponent<Text>().text = this.time.ToString("F1");
+        this.pointText.GetComponent<Text>().text = this.point.ToString() + " point";
+    }
+
+    public void GetApple()
+    {
+        this.point += 100;
+    }
+
+    public void GetBomb()
+    {
+        this.point /= 2;
+    }
+}
